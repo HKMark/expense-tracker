d38f586f4a9c5d8adac676ecdf6f418ce14fe54b
Mark Law <hongkongmark@gmail.com>
2023-02-19 23:35:42 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
 require('./config/mongoose')
 
@@ -8,11 +9,29 @@ const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
     .catch(error => console.error(error))
+})
+
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  console.log(req.body)
+  const name = req.body.name
+  const date = req.body.date
+  const amount = req.body.amount
+  const userId = req.user._id
+  const categoryId = req.body.category
+  return Record.create({ name, date, amount, userId, categoryId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
