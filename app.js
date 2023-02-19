2efe3c22dc25fb5e646bd1c5b4f3e84b440b84c7
Mark Law <hongkongmark@gmail.com>
2023-02-19 22:40:15 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
 require('./config/mongoose')
 
 const app = express()
@@ -8,7 +9,10 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
