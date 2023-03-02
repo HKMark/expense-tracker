b841edea31dcee85b6bb262263774a8023809765
HKMark <HongKongMark@Gmail.com>
2023-03-02 19:51:04 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -16,32 +16,50 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.findOne({ email }).then(user => {
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  try {
+    const user = await User.findOne({ email })
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
     }
-  })
-    .catch(err => console.log(err))
+    await User.create({
+      name,
+      email,
+      password
+    })
+    res.redirect('/')
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
